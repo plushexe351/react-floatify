21646c9e1c13ca4c8e9b8b8f36826796886e130f
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2025-09-13 21:12:17 +0530

    update package.json, index.ts to import css, vite.config.js

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
+import "./Toasty.scss";
 export { ToastProvider, useToast } from "./ToastContext";
 export { default as Toasty } from "./Toasty";
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: 'ReactFloatify',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'framer-motion', 'lucide-react'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
