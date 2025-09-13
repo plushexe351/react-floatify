e40e95977fc49b04383ba9365e1c2793543bfdba
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2025-09-13 21:44:19 +0530

    export types from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import "./Toasty.scss";
-export { ToastProvider, useToast } from "./ToastContext";
+export { ToastProvider, useToast, type ToastType, type ToastVariant } from "./ToastContext";
 export { default as Toasty } from "./Toasty";
